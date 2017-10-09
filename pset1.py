c0c0ef275cd17393acb9ed8786c9dbfcfb64206d
Tom O'Connell <toconnel@caltech.edu>
2017-10-08 22:01:57 -0700

    Initial commit. Trying to change the definition of the derivative operator somewhat to make it only compute terms that would be defined as per formula in pset.

---
+++ pset1.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import numpy as np
+
+def differentiation_operator(n, dx):
+    # TODO make this only calculate terms that would be fully
+    # defined
+    A = np.zeros((n, n+1))
+    #A = np.zeros((n+1, n+1))
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            # assumes function starts at 0
+            if i == (j - 1):
+                A[i,j] = dx
+            elif i == (j + 1):
+                A[i,j] = -dx
+    return A
+
+
+# 5
+def differentiate(f, dx=1, boundary_handling='copy', verbose=False):
+    f = np.squeeze(f)
+    # end of range (which starts at and includes 0. includes a.)
+    a = f.shape[0] * dx
+    # number of steps (# points = # steps + 1)
+    n = f.size - 1
+
+    A = differentiation_operator(n, n / (2 * a))
+    g = np.dot(A, f)
+
+    if boundary_handling == 'copy':
+        # handle boundaries
+        g[0] = g[1]
+        g[-1] = g[-2]
+
+    # 'correct' is what numpy calls this method of boundary handling
+    elif boundary_handling == 'correct':
+        g = g[1:n]
+
+    elif boundary_handling == 'none':
+        pass
+
+    if verbose:
+        print 'f', f
+        print 'A:\n', A
+        print 'g', g
+        print ''
+
+    return g
+
+a = 4
+n = 4
+# testing with f(x)=x**2
+f = np.linspace(0, a, num=(n+1)) ** 2
+g = differentiate(f, verbose=True)
+
+# is A invertible (5d)?
+f1 = np.ones(5)
+#f1[0] = 0
+#f1[-1] = 0
+#f1[3] = 1
+c = 5
+f2 = np.array(f1) * c
+# TODO i thought this would work?
+#f2[1:-1] = f2[1:-1] + c
+#f2[3] = f2[3] + c
+g1 = differentiate(f1, boundary_handling='none', verbose=True)
+g2 = differentiate(f2, boundary_handling='none', verbose=True)
+
+print 'f1', f1
+print 'f2', f2
+print 'g1', g1
+print 'g2', g2
+print ''
+
+if np.allclose(g1, g2):
+    print 'found a counterexample. ' + \
+        'differentiation is not invertible!'
+else:
+    print 'differentiation may be invertible.'
+
+A = differentiation_operator(2, 1)
+print A
+
+def det2by2(M):
+    # should evaluate to 1 on I
+    return M[0,0] * M[1,1] - M[0,1] * M[1,0]
+
+print det2by2(A)
