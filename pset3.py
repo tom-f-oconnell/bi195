728f235db5d8ea363894602170ca6ea6d19509e0
Tom O'Connell <toconnel@caltech.edu>
2017-10-23 10:57:56 -0700

    pset3.py now just has code for calculating and displaying power spectra w/ library calls.

--- pset3.py
+++ pset3.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 
-# TODO make a vim function to insert all this scientific python boilerplate
 import numpy as np
-
-from sympy import symbols
-from sympy.plotting import plot
-# the imaginary unit
-from sympy import I
-# seems to also work correctly on complex numbers (Euclidean norm of 2-vector)
-from sympy.functions import Abs
+import pandas as pd
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-R, L, C, w = symbols('R L C w')
+sns.set_style('dark')
 
-# TODO i might have thought this was going to be non-negative, but it seems
-# that is not the case, unless I made a mistake?
-hw_abs_squared = (w**2) / Abs(I*w + R/L - w**2*R*C)**2
-hw_abs_squared_R = hw_abs_squared.subs(L, 1)
-hw_abs_squared_R = hw_abs_squared_R.subs(C, 1)
+df = pd.read_csv('data_pset3.csv')
 
-# TODO log / log Bode plots?
-plot(hw_abs_squared_R.subs(R, 1), (w, 0, 10), title='$R=1$')
-plot(hw_abs_squared_R.subs(R, 5), (w, 0, 10), title='$R=5$')
+data = df['x']
+times = df['secs']
+dt = times[1] - times[0]
 
-plot(hw_abs_squared.subs(R, 1).subs(L, 5).subs(C, 1), (w, 0, 10), title='$L=5, C=1$')
-plot(hw_abs_squared.subs(R, 1).subs(L, 1).subs(C, 5), (w, 0, 10), title='$L=1, C=5$')
+power = np.abs(np.fft.fft(data))**2
 
+# TODO what exactly is fftfreq doing?
 
+# d=sample spacing, specific units irrelevant
+freqs = np.fft.fftfreq(len(data), d=dt)
+nonnegative_indices = freqs > 0
+
+plt.semilogy(freqs[nonnegative_indices], power[nonnegative_indices])
+plt.title('Sound power in animal facility')
+plt.xlabel('Frequency ($Hz$)')
+plt.ylabel('Power')
+plt.savefig('sound_power_pset3.svg')
+plt.show()
